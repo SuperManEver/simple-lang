10ca7cf68ebdc15070f843b2a1444d2441b7a1ad
Nick Luparev <nikita.luparev@gmail.com>
2020-01-22 18:42:23 +0500

    configure jest

---
+++ src/tokenize.ts
@@ -0,0 +1,13 @@
+// const {
+//   isLetter,
+//   isWhitespace,
+//   isNumber,
+//   isParenthesis,
+//   isQuote,
+// } = require('./identify')
+
+export function tokenize (input: string) {
+  return input
+}
+
+// export default { tokenize }
